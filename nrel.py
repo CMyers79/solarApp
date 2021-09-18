2ae227e0bc8accd7bec2694e846f0f3cb7dbeed8
Dongwoo Kang <dongwookang@Dongwoos-MacBook-Pro.local>
2021-09-18 16:03:37 +0900

    combined arguments in a list

--- nrel.py
+++ nrel.py
@@ -6,9 +6,18 @@ headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache"
 }
-
-def get_GHI(latitude, longitude, s_Month, s_Day, s_Hour, s_Min, e_Month, e_Day, e_Hour, e_Min):
-    payload = "wkt=POINT("+str(longitude)+"%20"+str(latitude)+")&attributes=dhi%2Cdni%2Cghi%2Cdew_point%2Cair_temperature%2Csurface_pressure%2Cwind_direction%2Cwind_speed%2Csurface_albedo%2C%2C%2C&names=tmy-2020&full_name=Dongwoo%Kang&email=kangdo@oregonstate.edu&affiliation=OregonStateUniversity&mailing_list=false&reason=test&utc=true"
+""" coord[0] = latitude
+coord[1] = longitude
+s_Date[0] = s_Month
+s_Date[1] = s_Day
+s_Date[2] = s_Hour
+s_Date[3] = s_Min
+e_Date[0] = e_Month
+e_Date[1] = e_Day
+e_Date[2] = e_Hour
+e_Date[3] = e_Min """
+def get_GHI(coord, s_Date, e_Date):
+    payload = "wkt=POINT("+str(coord[0])+"%20"+str(coord[1])+")&attributes=dhi%2Cdni%2Cghi%2Cdew_point%2Cair_temperature%2Csurface_pressure%2Cwind_direction%2Cwind_speed%2Csurface_albedo%2C%2C%2C&names=tmy-2020&full_name=Dongwoo%Kang&email=kangdo@oregonstate.edu&affiliation=OregonStateUniversity&mailing_list=false&reason=test&utc=true"
     response = requests.request("POST", url, data=payload, headers=headers)
     print(response)
     file1 = open("corvallis.csv","w")
@@ -17,7 +26,7 @@ def get_GHI(latitude, longitude, s_Month, s_Day, s_Hour, s_Min, e_Month, e_Day, 
         csv_reader = csv.DictReader(csv_file)
         csv_reader.fieldnames = "Year" ,"Month","Day","Hour","Minute","DHI","DNI","GHI","Dew Point","Temperature","Pressure","Wind Direction","Wind Speed","Surface Albedo"
         for line in csv_reader:
-            if line['Month'] == s_Month and line['Day'] == s_Day and line['Hour'] == str(int(s_Hour)-1) and line['Minute'] == s_Min:
+            if line['Month'] == s_Date[0] and line['Day'] == s_Date[1] and line['Hour'] == str(int(s_Date[2])-1) and line['Minute'] == s_Date[3]:
                 for line in csv_reader: 
                     print("-----------------------------------")
                     print("Month: " + line['Month'])
@@ -26,7 +35,7 @@ def get_GHI(latitude, longitude, s_Month, s_Day, s_Hour, s_Min, e_Month, e_Day, 
                     print("Minute: " + line['Minute'])
                     print(" GHI: " + line['GHI']) 
                     list_GHI.append(line['GHI'])
-                    if line['Month'] == e_Month and line['Day'] == e_Day and line['Hour'] == e_Hour and line['Minute'] == e_Min:
+                    if line['Month'] == e_Date[0] and line['Day'] == e_Date[1] and line['Hour'] == str(int(s_Date[2])-1) and line['Minute'] == s_Date[3]:
                         break
     return list_GHI
 
