0ba62fa57666b4c3ff1fde58fde7eed94198c16b
CMyers79 <myerscod@oregonstate.edu>
2021-09-18 22:18:29 -0700

    update all files

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 from nrel import get_GHI
+from calcs import track_battery_changes, generate_graph
 import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1ED964D2E87CF13129B56A5F4B86A'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -20,16 +22,17 @@ def index():
         elif not startdate or not enddate:
             flash('Start and end dates are required')
         else:
-
             startdate = [int(numString) for numString in startdate.split("/")]
             enddate = [int(numString) for numString in enddate.split("/")]
             coords = [float(latitude), float(longitude)]
             session['startdate'] = startdate
             session['enddate'] = enddate
             session['coords'] = coords
-            return redirect(url_for('result1', startdate=startdate, enddate=enddate, coords=coords))
+
+            return redirect(url_for('result1', startdate=startdate, enddate=enddate, coords=coords), code=307)
 
     return render_template('index.html')
+
 
 @app.route('/result1', methods=['GET', 'POST'])
 def result1():
@@ -38,17 +41,46 @@ def result1():
     coords = session['coords']
 
     GHI_list = get_GHI(coords, startdate, enddate)
-    print(GHI_list)
 
     if request.method == 'POST':
-        nextpage = request.form['nextpage']
-        if nextpage is not None:
-            return redirect(url_for('index2'))
-        return render_template('result1.html')
+        if "continue" in request.form:
+            return redirect(url_for('index2'), code=307)
+        return render_template('result1.html', list1=GHI_list)
+
+
+@app.route('/index2', methods=['GET', 'POST'])
+def index2():
+    if request.method == 'POST':
+        water = request.form['water']
+        fan = request.form['fan']
+        lighting = request.form['lighting']
+        plug_load = request.form['plug_load']
+        refrig = request.form['refrig']
+
+    if not water or not fan or not lighting or not plug_load or not refrig:
+        flash('All inputs are required, enter 0 for no consumption')
+    else:
+        water = int(water) / 2
+        fan = int(fan)
+        lighting = int(lighting)
+        plug_load = int(plug_load)
+        refrig = int(refrig)
+        session['water'] = water
+        session['fan'] = fan
+        session['lighting'] = lighting
+        session['plug_load'] = plug_load
+        session['refrig'] = refrig
+
+        return redirect(url_for('result2', water=water, fan=fan, lighting=lighting, plug_load=plug_load, refrig=refrig),
+                        code=307)
+
+    return render_template('index2.html')
+
 
 @app.route('/result2', methods=['GET', 'POST'])
 def result2():
     return render_template('result2.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- nrel.py
+++ nrel.py
@@ -1,42 +1,27 @@
 import requests
 import csv
+
 url = "https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-tmy-download.csv?api_key=DEMO_KEY"
 list_GHI = []
 headers = {
     'content-type': "application/x-www-form-urlencoded",
     'cache-control': "no-cache"
 }
-""" coord[0] = latitude
-coord[1] = longitude
-s_Date[0] = s_Month
-s_Date[1] = s_Day
-s_Date[2] = s_Hour
-e_Date[0] = e_Month
-e_Date[1] = e_Day
-e_Date[2] = e_Hour
- """
+
+
 def get_GHI(coord, s_Date, e_Date):
-    payload = "wkt=POINT("+str(coord[0])+"%20"+str(coord[1])+")&attributes=dhi%2Cdni%2Cghi%2Cdew_point%2Cair_temperature%2Csurface_pressure%2Cwind_direction%2Cwind_speed%2Csurface_albedo%2C%2C%2C&names=tmy-2020&full_name=Dongwoo%Kang&email=kangdo@oregonstate.edu&affiliation=OregonStateUniversity&mailing_list=false&reason=test&utc=false"
+    payload = "wkt=POINT("+str(coord[1])+"%20"+str(coord[0])+")&attributes=dhi%2Cdni%2Cghi%2Cdew_point%2Cair_temperature%2Csurface_pressure%2Cwind_direction%2Cwind_speed%2Csurface_albedo%2C%2C%2C&names=tmy-2020&full_name=Dongwoo%Kang&email=kangdo@oregonstate.edu&affiliation=OregonStateUniversity&mailing_list=false&reason=test&utc=false"
     response = requests.request("POST", url, data=payload, headers=headers)
-    print(response)
+
     file1 = open("corvallis.csv","w")
     file1.write(response.text)
     with open('corvallis.csv', 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        csv_reader.fieldnames = "Year" ,"Month","Day","Hour","Minute","DHI","DNI","GHI","Dew Point","Temperature","Pressure","Wind Direction","Wind Speed","Surface Albedo"
+        csv_reader.fieldnames = "Year", "Month", "Day", "Hour", "Minute", "DHI", "DNI", "GHI", "Dew Point", "Temperature", "Pressure", "Wind Direction", "Wind Speed", "Surface Albedo"
         for line in csv_reader:
-            if line['Month'] == s_Date[0] and line['Day'] == s_Date[1] and line['Hour'] == str(int(s_Date[2])-1):
+            if line['Month'] == str(s_Date[0]) and line['Day'] == str(s_Date[1]) and line['Hour'] == str(int(s_Date[2]) - 1):
                 for line in csv_reader: 
                     list_GHI.append(line['GHI'])
-                    if line['Month'] == e_Date[0] and line['Day'] == e_Date[1] and line['Hour'] == str(int(s_Date[2])-1):
+                    if line['Month'] == str(e_Date[0]) and line['Day'] == str(e_Date[1]) and line['Hour'] == str(int(s_Date[2]) - 1):
                         break
     return list_GHI
-
-#get_GHI(latitude = "44",longitude = "-123", s_Month = "1", s_Day = "1", s_Hour = "14", s_Min = "30", e_Month = "1", e_Day = "1", e_Hour = "16", e_Min = "30")
-""" for val in list_GHI:
-    print(val) """
-
-
-
-
-
